48d0f6522753709b1167cf2b109981c5323db057
Jim Gay <jim@saturnflyer.com>
2009-11-19 16:14:11 -0500

    update edit screens for proper overflow, display indices with type columns

--- file_system_resources_extension.rb
+++ file_system_resources_extension.rb
@@ -9,5 +9,11 @@ class FileSystemResourcesExtension < Radiant::Extension
   def activate
     Layout.send(:include, FileSystemResource)
     Snippet.send(:include, FileSystemResource)
+    
+    # Radiant chokes on these files...
+    # admin.layouts.index.add :thead, 'layout_type_head', :before => 'modify_header'
+    # admin.layouts.index.add :tbody, 'layout_type_column', :before => 'modify_cell'
+    # admin.snippets.index.add :thead, 'snippet_type_head', :before => 'modify_header'
+    # admin.snippets.index.add :tbody, 'snippet_type_column', :before => 'modify_cell'
   end
 end
